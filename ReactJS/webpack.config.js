f5f3745b086f9f3804fcb4dfd73959fcf35c9fee
jmakoff <juramak98@gmail.com>
2016-09-08 23:35:00 +0300

    update

---
+++ ReactJS/webpack.config.js
@@ -0,0 +1,26 @@
+﻿/// <binding ProjectOpened='Watch - Development' /> 
+"use strict";
+
+module.exports = {
+    entry: {
+        "les19/homework1/build/bundle": "./les19/homework1/jsx/main.jsx"
+
+
+    } ,
+           
+    output: {
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                query:
+                {
+                       presets: ['react']
+                }
+            }
+        ]
+    }
+};
