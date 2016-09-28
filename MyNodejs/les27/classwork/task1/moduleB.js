541228e7389d3ee2ae85ca22c32248e85adf0eac
juramak <juramak98@gmail.com>
2016-09-28 13:27:56 +0300

    les 27+ 28 cw

---
+++ MyNodejs/les27/classwork/task1/moduleB.js
@@ -0,0 +1,9 @@
+/*###Задача 1
+ * Создайте два модуля – A и B(в отдельных файлах).
+ * Модуль A должен содержать функцию sum, прин /home/juramak/Documents/Ignite/004_Node+
+ имающую 2 аргумента и выводящую их сумму в консоль.
+ */
+var modA = require('./moduleA')
+var arg1 = 100;
+var arg2 = 200;
+modA.summ(arg1,arg2)
---
+++ MyNodejs/les27/homework/task1/moduleB.js
@@ -0,0 +1,3 @@
+var loaded_module = require('./moduleA');
+console.log(loaded_module.a);
+console.log(loaded_module.b(loaded_module.a));
