6b7b9736402b02a1871fe7eb2d73f3f38bbb819a
juramak <juramak98@gmail.com>
2016-10-11 17:02:44 +0300

    upd

---
+++ MyNodejs/project/pages/javascript/main.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+    var homeBtn = document.getElementById('home');
+    var contactBtn = document.getElementById('contactbtn')
+    contactBtn.onclick = function () {
+        window.location.pathname = '/contact'
+        console.log('successfully redirected to contact')
+    }
+    homeBtn.onclick = function () {
+        window.location.pathname = '/home'
+        console.log('successfully redirected to home')
+    }
+    /*Disable/enable button LogIn*/
+    document.getElementById('login').onchange = function () {
+        if (document.getElementById('password').value.length > 0) {
+            if (document.getElementById('login').value.length > 0) {
+                document.getElementById('log').disabled = false
+            }
+        }
+    }
+    document.getElementById('password').onchange = function () {
+        if (document.getElementById('login').value.length > 0) {
+            if (document.getElementById('password').value.length > 0) {
+                document.getElementById('log').disabled = false
+            }
+        }
+    }
+    document.getElementById('log').onclick = function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/login');
+        var send = {
+            login:  document.getElementById('login').value,
+            password:  document.getElementById('password').value
+        }
+        var data = JSON.stringify(send)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(data)        //send data
+
+        xhr.onload= function () {
+            alert(this.responseText);
+        }
+        document.getElementById('password').value = "";//cleanup after yourself
+        document.getElementById('login').value = "";
+        console.log('POST send made!')
+
+
+    }
+
+}
--- MyNodejs/project/server.js
+++ MyNodejs/project/server.js
@@ -2,13 +2,31 @@ var express  = require('express');
 var app = express();
 var path = require('path');
 var port = process.env.port || 1337;
+var bodyParser = require('body-parser');
 
 var mysql = require('mysql');
+var jsonParser = bodyParser.json();
+var textParser = bodyParser.text();
+
+app.use(jsonParser);
+app.use(textParser);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('pages'));
+
 app.get('/', function(req, res){
     res.redirect('/home');
 })
 app.get('/home', function (req, res) {
     res.sendFile(path.join(__dirname, "pages/home.html"))
+})
+app.post('/login', function (req, res) {
+if (req.body.login==="admin" && req.body.password =='12345'){
+    res.send('You`ve been successfully authorised as '+ req.body.login)
+}
+else {
+    res.send('Authorisation problem!')
+}
+
 })
 
 app.listen(port, function () {
